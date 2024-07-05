0f831c48d02958e16ebf58f95f41e3eee068ad63
Charles Jester <charles.jester@revature.com>
2024-07-05 17:55:40 -0400

    added some new testing before the end of the week for everyone to see

---
+++ src/main/java/com/revature/ams/util/exceptions/NoSpaceException.java
@@ -0,0 +1,7 @@
+package com.revature.ams.util.exceptions;
+
+public class NoSpaceException extends RuntimeException{
+    public NoSpaceException(String message) {
+        super(message);
+    }
+}
