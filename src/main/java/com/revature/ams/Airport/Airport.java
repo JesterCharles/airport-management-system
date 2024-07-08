c80c909f8b45f8f0e797c762c8aaa410713b2a0f
Charles Jester <charles.jester@revature.com>
2024-07-08 17:36:08 -0400

    updated for airline & airport for SQL discussions tomorrow

--- src/main/java/com/revature/ams/Airport/Airport.java
+++ src/main/java/com/revature/ams/Airport/Airport.java
@@ -1,4 +1,9 @@
 package com.revature.ams.Airport;
 
 public class Airport {
+    private String airportCode;
+    private String name;
+    private String city;
+    private String country;
+    private String address;
 }
