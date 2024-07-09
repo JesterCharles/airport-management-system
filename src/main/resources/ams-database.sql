9c73913d4bc34d5145a0f41e24676ba450e4f2b7
Charles Jester <charles.jester@revature.com>
2024-07-09 16:20:13 -0400

    added in the sql file updates for working with our database

---
+++ src/main/resources/ams-database.sql
@@ -0,0 +1,34 @@
+-- CRUD - Create, Read, Update, Delete (soft/hard)
+-- shortcut: ctrl+enter will run a single SQL statement
+-- drops
+drop table flights;
+
+-- CREATE - Keywords: create, insert, alter
+create table flights(
+	flight_number integer primary key check (flight_number > 1000 and flight_number < 999999),
+	origin_airport varchar(3) not null, -- foreign keys, use alter
+	destination_airport varchar(3) not null,
+	time_departure date,
+	time_arrival date,
+	seat_count smallint not null check (seat_count > 0),
+	pilot integer check (pilot > 100000 and pilot < 999999),
+	airline integer
+); 
+
+insert into 
+	flights(flight_number, origin_airport, destination_airport, seat_count)
+values (1234,'PHL','BOS',12);
+
+insert into 
+	flights(flight_number, origin_airport, destination_airport, seat_count)
+values (123456,'PHL','BOS',12);
+
+-- READ
+select * from flights;
+
+-- UPDATE WHENVER you update make sure you have a WHERE clause
+update flights
+	set destination_airport= 'NYC'
+	where flight_number = 1234;
+
+-- DELETE
