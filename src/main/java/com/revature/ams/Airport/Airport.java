ca2ff1bc9fe3ad5d7b5854775cb403529f70c1d8
Charles Jester <charles.jester@revature.com>
2024-07-15 13:57:44 -0400

    updated the architecture diagam for our application

--- src/main/java/com/revature/ams/Airport/Airport.java
+++ src/main/java/com/revature/ams/Airport/Airport.java
@@ -6,4 +6,66 @@ public class Airport {
     private String city;
     private String country;
     private String address;
+
+    public Airport() {
+    }
+
+    public Airport(String airportCode, String name, String city, String country, String address) {
+        this.airportCode = airportCode;
+        this.name = name;
+        this.city = city;
+        this.country = country;
+        this.address = address;
+    }
+
+    public String getAirportCode() {
+        return airportCode;
+    }
+
+    public void setAirportCode(String airportCode) {
+        this.airportCode = airportCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    @Override
+    public String toString() {
+        return "Airport{" +
+                "airportCode='" + airportCode + '\'' +
+                ", name='" + name + '\'' +
+                ", city='" + city + '\'' +
+                ", country='" + country + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
 }
