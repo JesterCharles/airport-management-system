9111f6958351bd1e74aab477698b866af61e89c7
rubensamf <ruben146@revature.net>
2024-07-19 15:18:25 -0500

    Wrote documentation for the caclulateTotalPrice method and the two helper methods it uses, calculateSeatPrice and calculateLuggagePrice
    Added comments for the @parameter and @return

--- src/main/java/com/revature/ams/Booking/BookingService.java
+++ src/main/java/com/revature/ams/Booking/BookingService.java
@@ -81,6 +81,110 @@ public class BookingService {
             default -> throw new IllegalArgumentException("Invalid seat type");
         };
     }
+package com.revature.ams.Booking;
+
+import com.revature.ams.Booking.dtos.BookingResponseDTO;
+import com.revature.ams.util.exceptions.InvalidInputException;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+import java.util.Optional;
+
+    /**
+     * TODO: DOCUMENT ME
+     */
+    public class BookingService {
+        private final BookingRepository bookingRepository;
+
+        public BookingService(BookingRepository bookingRepository) {
+            this.bookingRepository = bookingRepository;
+        }
+
+        /**
+         * TODO: DOCUMENT ME
+         *
+         * @param newBooking
+         * @return
+         */
+        public BookingResponseDTO bookFlight(Booking newBooking) {
+            newBooking.setPrice(calculateTotalPrice(newBooking));
+            if(newBooking.getSeatType().name().equals("BUSINESS") || newBooking.getSeatType().name().equals("FIRSTCLASS")){
+                newBooking.setCarryOnAllowed(true);
+            }
+
+            Optional<Booking> booking = bookingRepository.create(newBooking);
+            booking.orElseThrow(() -> new InvalidInputException("Double-Check "));
+
+            return booking.map(BookingResponseDTO::new).get();
+        }
+
+        /**
+         * TODO: DOCUMENT ME
+         * @return
+         */
+        public List<Booking> findAll() {
+            return bookingRepository.findAll();
+        }
+
+        /**
+         * TODO: DOCUMENT ME
+         * @param memberId
+         * @return
+         */
+        public List<BookingResponseDTO> findAllBookingsByMemberId(int memberId){
+            return bookingRepository.findAllBookingsByMemberId(memberId)
+                    .stream()
+                    .map(BookingResponseDTO::new)
+                    .toList();
+        }
+
+        /**
+         * Calculates total price of a ticket given seat price and luggage price
+         * @param booking The booking object that contains the seatType and checkedLuggage
+         * @return The total price of the ticket given seat price and luggage price
+         */
+        public BigDecimal calculateTotalPrice(Booking booking) {
+            BigDecimal seatPrice = calculateSeatPrice(booking.getSeatType());
+            BigDecimal luggagePrice = calculateLuggagePrice(booking.getCheckedLuggage());
+            return new BigDecimal("299.99").add(seatPrice).add(luggagePrice);
+        }
+
+        /**
+         * A helper method that returns the price of a given seat type
+         * @param seatType an enum used to choose which price to charge (e.g. Economy, Business, etc.)
+         * @return The price of the seat
+         */
+        private BigDecimal calculateSeatPrice(Booking.SeatType seatType) {
+            return switch (seatType) {
+                case SEATSOPTIONAL -> new BigDecimal("50.00");
+                case ECONOMY -> new BigDecimal("150.00");
+                case BUSINESS -> new BigDecimal("400.00");
+                case FIRSTCLASS -> new BigDecimal("1000.00");
+                default -> throw new IllegalArgumentException("Invalid seat type");
+            };
+        }
+
+        /**
+         * A helper method that calculates luggage price given the number of checked luggage minus a discount
+         * @param checkedLuggage The amount of checked luggage
+         * @return The luggage price minus a discount
+         */
+        private BigDecimal calculateLuggagePrice(short checkedLuggage) {
+            if (checkedLuggage < 0) {
+                throw new IllegalArgumentException("Number of checked luggage cannot be negative");
+            }
+
+            checkedLuggage = (short) Math.min(checkedLuggage, (short) 4);
+
+            BigDecimal baseLuggagePrice = new BigDecimal(checkedLuggage * 30.00);
+
+            BigDecimal discountMultiplier = BigDecimal.ONE.subtract(new BigDecimal(checkedLuggage).divide(new BigDecimal(4), 2, RoundingMode.HALF_UP));
+
+            return baseLuggagePrice.multiply(discountMultiplier);
+        }
+
+    }
 
     /**
      * TODO: DOCUMENT ME
